3d40445b3e8436fb1193bdf1eddd3ef4c04d1ef1
lamare559 <63024892+lamare559@users.noreply.github.com>
2021-04-28 15:21:18 +0100

    First commit

---
+++ resources/views/pizzas/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+    <form action="/pizzas" method="POST">
+        @csrf
+        <label for="name">Your name:</label>
+        <input type="text" id="name" name="name">
+        <label for="type">Choose pizza type</label>
+        <select name="type" id="type">
+            <option value="margarita">Margarita</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="veg supreme">Veg Supreme</option>
+            <option value="volcano">Volcano</option>
+        </select>
+        <label for="type">Choose base type</label>
+        <select name="base" id="base">
+            <option value="cheezy crust">Cheezy Crust</option>
+            <option value="garlic crust">Garlic Crust</option>
+            <option value="thin & crispy">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+        <fieldset>
+            <label>Extra toppings:</label>
+            <input type="checkbox" name="toppings[]" id="toppings" value="mushroom">Mushroom<br />
+            <input type="checkbox" name="toppings[]" id="toppings" value="peppers">Peppers<br />
+            <input type="checkbox" name="toppings[]" id="toppings" value="garlic">Garlic<br />
+            <input type="checkbox" name="toppings[]" id="toppings" value="olives">Olives<br />
+        </fieldset>
+        <input type="submit" value="Order Pizza">
+    </form>
+</div>
+@endsection
